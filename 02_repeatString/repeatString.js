4be47abba413d32c36d47c483562fd48b1b9ebf3
tan-sahil <tha.sahu@gmail.com>
2023-08-02 01:52:10 -0700

    changed 2 files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word , times) {
+    if(word< 0) return "Error";
+    let ans ="";
+    for(let i = 0; i< times ; i++){
+        ans +=word ;
+    }
+    return ans;
 };
 
 // Do not edit below this line
