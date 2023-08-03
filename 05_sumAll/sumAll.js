138bf935027081125b7149bf5de5ec2fc113b68d
tan-sahil <tha.sahu@gmail.com>
2023-08-02 23:24:19 -0700

    added some logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+  
+  if(typeof(first) !== "number" || typeof(last) !== "number" ){
+    return "ERROR"
+  }
+  if(last <0 || first < 0) return "Error";
+  
+  if(first > last){
+    let temp = first;
+    first = last;
+    last = temp;
+  }
+   let totalSum = 0;
+    for(let i = first; i<= last; i++){
+        totalSum+= i;
+    }
+    return totalSum;
+  
 };
 
 // Do not edit below this line
